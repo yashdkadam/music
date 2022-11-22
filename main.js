ddb652b642df53f2c6aae20d42ea06065d0717f1
Yash Kadam <kadamdyash55@gmail.com>
2022-11-22 21:05:13 +0530

    commit

---
+++ main.js
@@ -0,0 +1,67 @@
+if(localStorage.getItem('ind') != undefined || localStorage.getItem('ind') != NaN){
+    var ind = localStorage.getItem('ind');
+} else{
+    ind = Math.floor(Math.random()*localStorage.getItem('size'));
+}
+async function getMp3Url(q) {
+    let res = await $.get({url:'https://apiyoutube.cc/check.php?callback=jQuery34108672477917974195_1666787018121&v=' + q, Cache: false});
+    // let data = Object.values(res);
+    var i = 1;
+    res = res.split('"')
+    arr = []
+    for (index in res) {
+        if (res[index].includes('::')) {
+            arr.push(res[index]);
+        }
+    }
+    mp3Url = "https://apiyoutube.cc/m4a/" + arr[1] + "::" + arr[0];
+    return mp3Url;
+}
+
+track_list = [];
+async function getTrackResponse() {
+  const res = await $.get(
+    "https://shrouded-escarpment-70092.herokuapp.com/api/songs"
+  );
+  let data = Object.values(res);
+  var i = 1;
+  //   console.log(data);
+  // data.reverse();
+  for (index in data) {
+    track_list.push(data[index]);
+  }
+  console.log(track_list);
+}
+getTrackResponse();
+let loop = setInterval(() => {
+  console.log("wait");
+  if (track_list.length > 0) {
+    start();
+    clearInterval(loop);
+  }
+}, 200);
+
+
+async function start() {
+  let url = await getMp3Url(track_list[ind]["url"]);
+  $('#src').attr('src', url);
+  $('.player').eq(0).remove();
+  $('body').append(`
+  <div id=${ind} class="player">
+  <img src=${track_list[ind]['imgUrl']} alt="" id="image">
+    <h4>${track_list[ind]['name']}</h4>
+  <audio controls autoplay name="media" onended="start()" id>
+  <source src=${url} type="audio/webm" id="src">
+</audio>
+</div>`)
+  console.log(url);
+  if(localStorage.getItem('size') != undefined){
+      var size = localStorage.getItem('size');
+      ind = Math.floor(Math.random()*track_list.length);
+    } 
+    localStorage.setItem('ind', Math.floor(Math.random()*track_list.length));
+    localStorage.setItem('size', track_list.length);
+
+  console.log(ind);
+}
+
